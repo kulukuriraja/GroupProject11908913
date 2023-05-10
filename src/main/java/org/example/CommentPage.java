9bcfd8efb13b73d158a9fd8dfaa74eaa1abe8dc6
Raja <kulukuriraja@gmail.com>
2023-05-10 11:32:10 +0530

    1

--- src/main/java/org/example/CommentPage.java
+++ src/main/java/org/example/CommentPage.java
@@ -7,7 +7,7 @@ public class CommentPage {
     @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/article[1]/div[1]/div[1]/div[3]/div[7]/div[1]/div[3]/div[1]/div[1]/div[1]/*[name()='svg'][1]")
     WebElement like;
 
-    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[2]/div[2]/div[1]/span[1]/span[1]")
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/div[2]/div[1]/span[1]/span[1]")
     WebElement reply;
 
     @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/main[1]/div[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/article[1]/div[1]/div[1]/div[3]/div[7]/div[1]/div[1]/div[1]/div[1]/div[1]/*[name()='svg'][1]")
